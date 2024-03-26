d2d309f4069f5bbec8e443c96e95f7492111e137
HeesikK <7jjhkhz@naver.com>
2024-03-26 19:44:01 +0900

    path1: pagination 뒤로가기 구현중

--- path1/pagination/src/components/pagination.jsx
+++ path1/pagination/src/components/pagination.jsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Pagination = ({ user, setUserList, limit, pagePerUser }) => {
   // 총 페이지 수
   const totalPage = Math.ceil(user.length / pagePerUser);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = searchParams.get("page") ?? "1";
+
+  const [currentPage, setCurrentPage] = useState();
+
+  console.log("파람값은?", params);
 
   // 랜더링되는 페이지 그룹
   const [pageGroup, setPageGroup] = useState(1);
 
   const totalPageArray = Array.from({ length: totalPage }, (_, idx) => idx + 1);
   const paginationArray = Array.from({ length: Math.ceil(totalPage / limit) }, (_, idx) => totalPageArray.slice(idx * limit, idx * limit + limit));
-
-  console.log("paginationArray값은?", paginationArray);
 
   // "<<" 버튼 클릭
   const onFirstPage = () => {
@@ -35,8 +41,12 @@ const Pagination = ({ user, setUserList, limit, pagePerUser }) => {
 
   // page 클릭
   const onMoveCurrentPage = (e) => {
-    setUserList(user.slice(pagePerUser * (e.target.textContent - 1), pagePerUser * e.target.textContent));
+    setSearchParams({ page: String(e.target.textContent) });
   };
+
+  useEffect(() => {
+    setUserList(user.slice(pagePerUser * (Number(params) - 1), pagePerUser * Number(params)));
+  }, [params]);
 
   /*
     "<<", "<", ">", ">>" 버튼을 클릭하면 해당 pageGroup의 첫 번째 페이지(인덱스)로 이동
