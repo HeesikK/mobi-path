db082aed7e6b314d5c775761d5439482490d262a
HeesikK <7jjhkhz@naver.com>
2024-04-22 09:14:42 +0900

    path3: 보일러 템플릿 - 디자인 시스템

--- path3/boiler-template/src/App.js
+++ path3/boiler-template/src/App.js
@@ -2,9 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import GlobalStyles from "./styles/global.style";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme.style";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
