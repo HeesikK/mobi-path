2c2edd4963293b611e3754e265185c56bd0e4504
HeesikK <7jjhkhz@naver.com>
2024-04-23 15:16:09 +0900

    path3: rhf - 라우터 설정

--- path3/rhf/src/App.js
+++ path3/rhf/src/App.js
@@ -1,19 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
+import GlobalStyles from "./style/global.style";
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme.style";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
 
---
+++ path3/rhf/src/router/router.jsx
@@ -0,0 +1,19 @@
+import HomePage from "../page/home/home";
+import SignIn from "../page/sign/sign-in";
+import SignUp from "../page/sign/sign-up";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+]);
