8df22ad18954ff6d207f70ba7bb68723affd176e
HeesikK <7jjhkhz@naver.com>
2024-04-24 11:55:07 +0900

    path3: rhf - sign in 폼 구현 완료(yup 스키마)

--- path3/rhf/src/consts/input-props.jsx
+++ path3/rhf/src/consts/input-props.jsx
@@ -1,3 +1,8 @@
+export const signInInputProps = [
+  { type: "email", name: "email", label: "아이디", placeholder: "아이디를 입력해주세요", variant: "primary", size: "large", shape: "shape" },
+  { type: "password", name: "password", label: "비밀번호", placeholder: "비밀번호를 입력해주세요", variant: "primary", size: "large", shape: "shape" },
+];
+
 export const signUpInputProps = [
   { type: "text", name: "nickname", label: "닉네임", placeholder: "닉네임을 입력해주세요", variant: "primary", size: "large", shape: "shape" },
   { type: "email", name: "email", label: "아이디", placeholder: "아이디를 입력해주세요", variant: "primary", size: "large", shape: "shape" },
--- path3/rhf/src/page/sign/sign-in.jsx
+++ path3/rhf/src/page/sign/sign-in.jsx
@@ -3,15 +3,30 @@ import { flexCenter } from "../../style/common.style";
 import CustomInput from "../../components/input";
 import CustomButton from "../../components/button";
 import { useForm } from "react-hook-form";
+import { schema } from "../../utils/schema";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { signInInputProps } from "../../consts/input-props";
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  const {} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, errors },
+  } = useForm({ mode: "onChange", resolver: yupResolver(schema) });
+
+  const navigate = useNavigate();
+
+  const onSubmitForm = () => {
+    navigate("/");
+  };
 
   return (
-    <Wrapper>
-      <CustomInput variant={"primary"} size={"large"} shape={"shape"} label={"아이디"} placeholder={"아이디를 입력해주세요"} />
-      <CustomInput variant={"primary"} size={"large"} shape={"shape"} label={"비밀번호"} placeholder={"비밀번호를 입력해주세요"} />
-      <CustomButton variant={"primary"} size={"large"} shape={"shape"}>
+    <Wrapper noValidate onSubmit={handleSubmit(onSubmitForm)}>
+      {signInInputProps.map((el) => (
+        <CustomInput type={el.type} name={el.name} label={el.label} placeholder={el.placeholder} variant={el.variant} size={el.size} shape={el.shape} register={register} errors={errors} />
+      ))}
+      <CustomButton disabled={!isValid} variant={"primary"} size={"large"} shape={"shape"}>
         로그인
       </CustomButton>
     </Wrapper>
@@ -19,7 +34,7 @@ const SignIn = () => {
 };
 export default SignIn;
 
-const Wrapper = styled.div`
+const Wrapper = styled.form`
   ${flexCenter}
   flex-direction: column;
   height: 100vh;
