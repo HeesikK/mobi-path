10303b7f64e74b810e1d8d974f4c04e426a1f35a
HeesikK <7jjhkhz@naver.com>
2024-04-27 23:45:29 +0900

    path3: di - 디자인 시스템 적용

---
+++ path3/di/src/styles/theme.style.jsx
@@ -0,0 +1,44 @@
+const COLORS = {
+  primary: {
+    blue: "#282190",
+    navy: "#100D45",
+    yellow: "#FFD02C",
+    beige: "#fceec5",
+  },
+  secondary: {
+    purple: "#a55ced",
+    pink: "#ff99cc",
+    mint: "#b3ffe3",
+  },
+  font: "#0F0F0F",
+  error: "#EC0707",
+  white: "#FFFFFF",
+  black: "#0F0F0F",
+  gray: {
+    100: "#F1F1F1",
+    200: "#D9D9D9",
+    300: "#C1C1C1",
+    400: "#757575",
+    500: "#5B5B5B",
+  },
+};
+
+const FONT_SIZE = {
+  small: "12px",
+  medium: "18px",
+  large: "24px",
+};
+
+const FONT_WEIGHT = {
+  thin: 400,
+  regular: 500,
+  bold: 700,
+};
+
+const theme = {
+  COLORS,
+  FONT_SIZE,
+  FONT_WEIGHT,
+};
+
+export default theme;
